67068d84ab1d808545f38e91061a5ac5e291b070
lucasbruno1992 <llucasbruno92@gmail.com>
2022-07-13 20:05:07 -0300

    Modificacion del Simulador Interactivo

--- app.js
+++ app.js
@@ -1,32 +1,63 @@
-let precio = " "
-let total = " "
+let precio = parseInt(prompt(`Ingrese un precio: `))
+let total = ""
+let cantidadDeCuotas = ""
+let medioDePago = prompt(`Ingrese medio de pago: 
+efectivo;
+con tarjeta;`)
+let cuponDeDescuento = (prompt(`Ingrese cupon de descuento: `))
+let listado = ""
 
-function cuotas(precio, cantidadDeCuotas) {
-    switch (cantidadDeCuotas) {
-        case 1:
-            return (`En ${cantidadDeCuotas} cuota de $ ${precio}`);
+switch (medioDePago.toLowerCase()) {
+    case "efectivo":
+        if(cuponDeDescuento.toLowerCase() !== "promo1"){
+            total = precio;
+            alert(`Total de su compra es: ${total}`);
             break;
+        } else{
+            total = precio-100;
+            alert(`Total de su compra es: ${total}`);
+            break;
+        }
+    case "con tarjeta":
+        for (let i = 1; i<=4; i++){
+            total = precio/(i*3);
+            listado += `En ${i*3} cuotas de $${total} cada una` + "\n";
+        }
+        alert(listado);
+        alert (cuotas(parseInt(prompt(`Ingrese la cantidad de cuotas:
+3 cuotas;
+6 cuotas;
+9 cuotas;
+12 cuotas;`))));
+break;
+    default: alert(`No ingreso metodo de pago`);
+    alert(medioDePago);
+        break;
+}
+
+function cuotas(cantidadDeCuotas) {
+    switch (cantidadDeCuotas) {
         case 3:
-            return (`En ${cantidadDeCuotas} cuotas de $ ${precio/3} cada una`);
+            return alert(`En ${cantidadDeCuotas} cuotas el valor de cada cuota es de: $` + precio / 3);
             break;
         case 6:
-            return (`En ${cantidadDeCuotas} cuotas de $ ${precio/6} cada una`);
+            return alert(`En ${cantidadDeCuotas} cuotas el valor de cada cuota es de: $` +  precio / 6);
+            break;
+        case 9:
+            return alert(`En ${cantidadDeCuotas} cuotas el valor de cada cuota es de: $` + precio / 9);
             break;
         case 12:
-            return (`En ${cantidadDeCuotas} cuotas de $ ${precio/12} cada una`);
-            break;
-        case 18:
-            return (`En ${cantidadDeCuotas} cuotas de $ ${precio/18} cada una`);
+            return alert(`En ${cantidadDeCuotas} cuotas el valor de cada cuota es de: $` + precio / 12);
             break;
         default:
             alert(`No ingresó cantidad de cuotas validados.`);
+            alert (cuotas(parseInt(prompt(`Ingrese la cantidad de cuotas:
+    3 cuotas;
+    6 cuotas;
+    9 cuotas;
+    12 cuotas;`))));
+    break;
     }
 }
-
-alert (cuotas(parseInt(prompt(`Ingrese el precio: `)),parseInt(prompt(`Ingrese la cantidad de cuotas:
-1 cuota;
-3 cuotas;
-6 cuotas;
-12 cuotas;`))))
 
 
